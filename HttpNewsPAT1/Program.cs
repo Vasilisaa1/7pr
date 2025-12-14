38d5c397dff3cddc1ef75d6a2ac4f73b25c5bf59
vasilisaa <vasilisaveber7@gmail.com>
2025-12-15 00:18:58 +0500

    SingIn

--- HttpNewsPAT1/Program.cs
+++ HttpNewsPAT1/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace HttpNewsPAT1
 {
@@ -14,15 +16,50 @@ namespace HttpNewsPAT1
         {
             WebRequest request = WebRequest.Create("http://10.111.20.114/main.php");
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
             Console.WriteLine(response.StatusDescription);
             Stream dataStream = response.GetResponseStream();
+
             StreamReader reader = new StreamReader(dataStream);
             string responseFromServer = reader.ReadToEnd();
+
             Console.WriteLine(responseFromServer);
             reader.Close();
             dataStream.Close();
+
             response.Close();
             Console.Read();
+        }
+        public static void SingIn(string Login, string Password)
+        {
+
+            string url = "http://news.permaviat.ru/ajax/login.php";
+            Debug.WriteLine($"Выполняем запрос: {url}");
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CookieContainer = new CookieContainer();
+
+           
+            string postData = $"login={Login}&password={Password}";
+            byte[] Data = Encoding.ASCII.GetBytes(postData);
+            request.ContentLength = Data.Length;
+
+            
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(Data, 0, Data.Length);
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+          
+            Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+
+          
+            string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
+           
+            Console.WriteLine(responseFromServer);
         }
     }
 }
